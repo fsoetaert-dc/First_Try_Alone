f1f49614a1e9207841ad5047f4d650718a5f2c15
Frederik <frederik.soetaert@hotmail.com>
2026-04-27 09:41:19 +0200

    tech talk updated

--- MyExercises/TechTalkClasses/ClassThatUsesPerson.cs
+++ MyExercises/TechTalkClasses/ClassThatUsesPerson.cs
@@ -6,11 +6,14 @@ public class Hospital
 {
     private List<Person> ListOfPersons = []; // a field can also be a list
 
-    public void AddPersonToListOfPersons(Person person) //method of the class Hospital
-    //Person is the type of what parameter the constructor wants,
-    // and person is just the name of what the constructor uses
+    public void AddPersonToListOfPersons(Person person) // method of the class Hospital
+    // Person is the type of what parameter the constructor wants,
+    // and person is the parameter that the constructor uses
     // person could've also been named human or animal or whatever. 
     // person was just the most obvious name in this context. it doesn't have to have the same name of the type.
+    // if you call the methode eg: var person1 = new Person("Sarah", 50)
+    // Sarah and 60 are called arguments
+    // person1 is called an object (can also be called an instance ><not to be confused with instanced members of a class)
     {
         ListOfPersons.Add(person); // adds the person to the list
         return;
@@ -22,7 +25,7 @@ public class Hospital
     }
 
     public void RankLeastSickPersonFirst() // also a method, ranks the persons in the list from least sick to most sick
-    //a method doesn't have to have a parameter to execute something
+    // a method doesn't have to have a parameter to execute something 
     {
         ListOfPersons.Sort((a, b) => a.sickScore.CompareTo(b.sickScore));
     }
--- MyExercises/TechTalkClasses/ExampleOfPrimConstr.cs
+++ MyExercises/TechTalkClasses/ExampleOfPrimConstr.cs
@@ -1,7 +1,7 @@
 namespace MyExercises;
 
-public class Animal(string name, int age) // chose another name because you can't have 2 classes with the same name
+public class Human(string name, int age) // chose another name because you can't have 2 classes with the same name
 {
-    public string Name { get; } = name;
-    public int Age { get; } = age;
+    public string Name { get; } = name; // Name is here only readonly (get), not writable
+    public int Age { get; set; } = age; // Age is read and writable
 }
